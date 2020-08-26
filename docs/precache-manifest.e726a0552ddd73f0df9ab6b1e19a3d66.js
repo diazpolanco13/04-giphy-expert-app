abea67fd89278f860f9d613287c047ff1382648e
Carlos Diaz <diazpolanco13@gmail.com>
2020-08-26 19:49:12 -0400

    renombrando docs

---
+++ docs/precache-manifest.e726a0552ddd73f0df9ab6b1e19a3d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d479696b85a4f94d25337505dd3b921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5159acbfdb0906b28ae6",
+    "url": "/static/css/main.0ab49595.chunk.css"
+  },
+  {
+    "revision": "5601105571549a2d03ac",
+    "url": "/static/js/2.7b27910a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b27910a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5159acbfdb0906b28ae6",
+    "url": "/static/js/main.5d4ae2cc.chunk.js"
+  },
+  {
+    "revision": "a3b2fcc5df2de8be9eb4",
+    "url": "/static/js/runtime-main.d6be54b9.js"
+  }
+]);
